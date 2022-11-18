0c12669141d74c47a8652ecd574df2179298dd2c
Vladimir Plesco <pleshco@deeplace.md>
2022-11-18 15:32:56 +0200

    7 lessons

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+const { DateTime } = require("luxon");
+const Image = require("@11ty/eleventy-img")
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css")
   eleventyConfig.addPassthroughCopy("src/assets/images");
-  
+  eleventyConfig.addPassthroughCopy({ "src/robots.txt": "/robots.txt" });
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  eleventyConfig.addShortcode("currentDate", (date = DateTime.now()) => {
+    return date;
+  });
   eleventyConfig.addShortcode(
     "headers",
     (title,subtitle) =>
@@ -13,7 +21,12 @@ module.exports = function(eleventyConfig){
       return a.data.order - b.data.order;
     });
   });
-
+  eleventyConfig.addShortcode(
+    "headers",
+    (title, subtitle) =>
+      `<h1>${title}</h1>
+        <p>${subtitle}</p>`
+  );
   return {
     dir: {
       input: "src",
